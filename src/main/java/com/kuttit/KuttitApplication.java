d0d9a2a3d96dfd4ac36683fa9132440c83852cbe
dev-ayush101 <ayushmishraofficial.619@gmail.com>
2025-09-11 22:19:00 +0530

    Added Spring Project Structure

---
+++ src/main/java/com/kuttit/KuttitApplication.java
@@ -0,0 +1,13 @@
+package com.kuttit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuttitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuttitApplication.class, args);
+	}
+
+}
