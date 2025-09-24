66d0bcf9d7f9c2a50817621b50c0e9a3b94db5af
dev-ayush101 <ayushmishraofficial.619@gmail.com>
2025-09-25 00:05:06 +0530

    Created DTOs for Register, Login and response

---
+++ src/main/java/com/kuttit/dto/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.kuttit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    String token;
+    String email;
+    String username;
+
+}
--- src/main/java/com/kuttit/model/User.java
+++ src/main/java/com/kuttit/model/User.java
@@ -19,7 +19,7 @@ public class User {
     @Id
     private String id;
 
-    private String userName;
+    private String username;
     private String email;
     private String passwordHash;
     private LocalDateTime createdAt;
