0d743e23f3d7cdd69cab1ae10853068accb17f40
dev-ayush101 <ayushmishraofficial.619@gmail.com>
2025-09-24 11:59:55 +0530

    Created User Model and Repository

---
+++ src/main/java/com/kuttit/model/User.java
@@ -0,0 +1,26 @@
+package com.kuttit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private String id;
+
+    private String userName;
+    private String email;
+    private String passwordHash;
+    private LocalDateTime createdAt;
+}
